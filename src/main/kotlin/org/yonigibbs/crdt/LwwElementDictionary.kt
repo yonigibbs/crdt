1abd32f5ae95efa972561aaf61ee89b7d90742ef
Yoni Gibbs <39593145+yonigibbs@users.noreply.github.com>
2025-02-16 11:50:22 +0000

    Disambiguate between upserts and removals

--- src/main/kotlin/org/yonigibbs/crdt/LwwElementDictionary.kt
+++ src/main/kotlin/org/yonigibbs/crdt/LwwElementDictionary.kt
@@ -5,24 +5,35 @@ import java.time.Instant
 
 // TODO: think about thread-safety/concurrent access?
 // TODO: make whole thing immutable?
+// TODO: implement Map interface?
 
 open class LwwElementDictionary<Key, Value, Timestamp : Comparable<Timestamp>, PeerId : Comparable<PeerId>>(
     private val peerId: PeerId,
     private val getCurrentTimestamp: () -> Timestamp
 ) {
-    private data class Element<Value, Timestamp : Comparable<Timestamp>, PeerId : Comparable<PeerId>>(
-        val value: Value,
-        val timestamp: Timestamp,
+    private interface Action<Timestamp : Comparable<Timestamp>, PeerId : Comparable<PeerId>> {
+        val timestamp: Timestamp
         val peerId: PeerId
-    )
+    }
 
-    private val additions = mutableMapOf<Key, Element<Value, Timestamp, PeerId>>()
-    private val removals = mutableMapOf<Key, Element<Value, Timestamp, PeerId>>()
+    private data class Upsert<Value, Timestamp : Comparable<Timestamp>, PeerId : Comparable<PeerId>>(
+        val value: Value,
+        override val timestamp: Timestamp,
+        override val peerId: PeerId
+    ) : Action<Timestamp, PeerId>
+
+    private data class Removal<Timestamp : Comparable<Timestamp>, PeerId : Comparable<PeerId>>(
+        override val timestamp: Timestamp,
+        override val peerId: PeerId
+    ) : Action<Timestamp, PeerId>
+
+    private val upserts = mutableMapOf<Key, Upsert<Value, Timestamp, PeerId>>()
+    private val removals = mutableMapOf<Key, Removal<Timestamp, PeerId>>()
 
     operator fun get(key: Key): Value? {
         // If the addition element isn't present then early return null: doesn't matter if it's in the removals set or
         // not.
-        val additionElement = additions[key] ?: return null
+        val additionElement = upserts[key] ?: return null
 
         // If it's not in the removals set then we can just return the one from the additions set.
         val removalElement = removals[key] ?: return additionElement.value
@@ -33,22 +44,24 @@ open class LwwElementDictionary<Key, Value, Timestamp : Comparable<Timestamp>, P
     }
 
     operator fun set(key: Key, value: Value) {
-        val existingAddition = additions[key]
-        val newAddition = Element(value, getCurrentTimestamp(), peerId)
+        val existingAddition = upserts[key]
+        val newAddition = Upsert(value, getCurrentTimestamp(), peerId)
 
         if (existingAddition == null || newAddition.supersedes(existingAddition))
-            additions[key] = newAddition
+            upserts[key] = newAddition
     }
 
-//    operator fun minusAssign(key: Key) {
-//        val existingRemoval = removals[key]
-//        val newRemoval = Element(value, getCurrentTimestamp(), peerId)
-//
-//        if (existingAddition == null || newAddition.supersedes(existingAddition))
-//            additions[key] = newAddition
-//    }
+    fun remove(key: Key) {
+        val existingRemoval = removals[key]
+        val newRemoval = Removal(getCurrentTimestamp(), peerId)
 
-    private fun Element<Value, Timestamp, PeerId>.supersedes(other: Element<Value, Timestamp, PeerId>) =
+        if (existingRemoval == null || newRemoval.supersedes(existingRemoval))
+            removals[key] = newRemoval
+    }
+
+    operator fun minusAssign(key: Key) = remove(key)
+
+    private fun Action<Timestamp, PeerId>.supersedes(other: Action<Timestamp, PeerId>) =
         when {
             this.timestamp > other.timestamp -> true
             this.timestamp < other.timestamp -> false
